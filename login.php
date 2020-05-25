958910d432a34675138ed207c885c5d35dde3f1f
xavier <xavier21school@gmail.com>
2020-05-25 23:48:30 +0200

    nutteloze commit skyblock pc

--- login.php
+++ login.php
@@ -11,6 +11,7 @@
     </script>
 </head>
 <body>
+
     <div id="sidebar"></div>
     <div class="wrap">
         <form class="login-form" method="post" action="functions/login_functions.php">
@@ -18,22 +19,26 @@
                 <h3>Login Form</h3>
                 <p>Login om de database aan te passen</p>
             </div>
+
             <!--Username Input-->
             <div class="form-group">
                 <input type="text" class="form-input" name="username" id="username" placeholder="Username" required="required">
             </div>
+
             <!--Password Input-->
             <div class="form-group">
                 <input type="password" class="form-input" name="password" id="password" placeholder="password" required="required">
             </div>
+
             <!--Login Button-->
             <div class="form-group">
                 <button class="form-button" type="submit" value="Submit" name="login_submit" id="login_submit">Login</button>
             </div>
+            
             <div class="form-footer">
             Geen account? <a href="signup.php">Maak een account aan!</a>
             </div>
         </form>
-    </div><!--/.wrap-->
+    </div>
 </body>
 </html>
